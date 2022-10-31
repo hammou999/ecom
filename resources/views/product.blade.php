c42544d1b53f29f63355091aeb7430280834fcb9
hammou999 <hammou0@yahoo.com>
2022-10-31 22:21:35 +0100

    one8

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -7,7 +7,6 @@
 
 @section('css-file')
     <link rel="stylesheet" type="text/css" href="{{asset('css/product.css')}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     <link href="https://fonts.googleapis.com/css?family=Tajawal&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
@@ -148,38 +147,6 @@
         <h3 class="heading-primary">منتجات سوف تعجبك</h3>
     </div>
 
-
-    <div class="products-grid">
-        @for ($i = 0; $i < 10; $i++)
-            <div class="product-item">
-                <a href="https://farah369.com/products/aldo-alkhark" class="product-thumbnail"><img
-                        data-src="https://cdn.youcan.shop/stores/c82c2f4e8acf4b0c2fe997fc6aeea9e1/products/65S1EhmZ4v4a5vgng3i4Ndt6InkiwkeYy7c8wyBV_md.jpeg"
-                        alt=""
-                        src="https://cdn.youcan.shop/stores/c82c2f4e8acf4b0c2fe997fc6aeea9e1/products/65S1EhmZ4v4a5vgng3i4Ndt6InkiwkeYy7c8wyBV_md.jpeg"></a>
-                <div class="product-details">
-                    <div class="product-info"><h3 class="product-title"><a
-                                href="https://farah369.com/products/nafkh-alshfah-altbyaay-alraeaa">نافخ الشفاه
-                                الرائع</a>
-                        </h3>
-                        <div class="product-price">
-                            <span class="currency-value before">
-                                <span class="value">4500</span>
-                                <span class="currency">&nbsp;د.ج.‏</span>
-                            </span>
-                            <span class="currency-value after">
-                                <span class="value">2700</span>
-                                <span class="currency">&nbsp;د.ج.‏</span>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="product-actions">
-                        <a href="https://farah369.com/products/nafkh-alshfah-altbyaay-alraeaa"
-                           class="button small-button secondary-button"> شراء الآن</a>
-                    </div>
-                </div>
-            </div>
-        @endfor
-    </div>
 @endsection
 
 @section('script')
